570727843a587b64c1e2437665e75f1a1cefd454
Brennedith Garcia <brennedithg@gmail.com>
2017-04-26 19:11:17 -0600

    Added Playbook. Missing content.

--- src/components/Playbook.vue
+++ src/components/Playbook.vue
@@ -1,19 +1,42 @@
 <template lang="pug">
-  h1 Playbook :)
+  .row.top
+    .col
+      b-card(v-for="(list, step) in process" :key="step" show-header no-block)
+        div(slot="header" @click="toggle(step)") {{ step }}
+        b-collapse(:id="step")
+          ul
+            li(v-for="item in list") {{ item }}
 </template>
 
 <script>
+import { bButton, bCard, bCollapse } from 'bootstrap-vue/lib/components'
 
 export default {
   name: 'playbook',
+  components: { bButton, bCard, bCollapse },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      process: {
+        'Opening': ['A', 'B', 'C'],
+        'Probing': ['A', 'B', 'C'],
+        'Benefits': ['A', 'B', 'C'],
+        'Offer': ['A', 'B', 'C'],
+        'Close': ['A', 'B', 'C']
+      }
+    }
+  },
+  methods: {
+    toggle (sel) {
+      console.log('Hi', sel)
+      this.$root.$emit('collapse::toggle', sel)
     }
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+  .card-header *
+    cursor pointer
+  ul
+    margin 15px 0
 </style>
